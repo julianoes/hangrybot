7b1a4f7ca12ece261877396cfae292b906bf336b
Julian Oes <julian@oes.ch>
2018-03-02 08:36:37 +0100

    menucrawler: fix parsing edge case
    Of course 2 is part in 26, so that was a stupid check.

--- menucrawler.py
+++ menucrawler.py
@@ -66,8 +66,9 @@ class BackmarktCrawler(MenuCrawler):
         our_day_active = False
         soup = BeautifulSoup(request.text, 'html.parser')
         for h2 in soup.select('div#contentBlock-1 div h2'):
-            title_without_year = str.join(" ", h2.text.split(" ")[0:-1])
-            if str(day_of_month) in title_without_year \
+            day_in_german, date_in_german = h2.text.split(",")
+            day_of_month_parsed, _ = date_in_german.split(".")
+            if day_of_month == int(day_of_month_parsed) \
                     and str(year) in h2.text:
                 our_day_active = True
             elif our_day_active and str(year) in h2.text:
