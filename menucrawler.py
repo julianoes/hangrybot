0fa01912561f6e95421f5c4a8a3be55b5c18cbcd
Julian Oes <julian@oes.ch>
2018-08-07 11:35:47 +0200

    menucrawler: fixup Backmarkt crawler as well

--- menucrawler.py
+++ menucrawler.py
@@ -75,15 +75,16 @@ class BackmarktCrawler(MenuCrawler):
                 our_day_active = False
 
             if our_day_active:
-                for div in h2.parent.next_sibling.select('div div p'):
-                    if u"menü " in div.text or u"menu" in div.text:
-                        self.menus.append(Menu(div.text))
-                        continue
-                    if u"Menüsalat" in div.text or u"Menusalat" in div.text:
-                        continue
-                    # Don't add empty lines.
-                    if self.menus and div.text.strip():
-                        self.menus[-1].text += div.text + "\n"
+                for p in h2.parent.next_sibling.select('div div p'):
+                    for line in p.text.split('\n'):
+                        if u"menü " in line.lower() or u"menu" in line.lower():
+                            self.menus.append(Menu(line))
+                            continue
+                        if u"Menüsalat" in line or u"Menusalat" in line:
+                            continue
+                        # Don't add empty lines.
+                        if self.menus and line.strip():
+                            self.menus[-1].text += line + "\n"
 
 
 def main():
